6125a0965c1419d4eb28818076417afeb9d6146d
luancurti <luancurti@gmail.com>
2019-08-26 13:16:38 -0300

    chore(react-native-linear-gradient): Add support

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
+import RNLinearGradient from "react-native-linear-gradient";
+import * as Expo from "expo";
+
 import generateGradient from "./generator";
 
 export { generateGradient };
 
-export default ({ gradient, children, style }) => {
-  const LinearGradient = require("expo").LinearGradient;
+const LinearGradientCss = ({ gradient, children, style }) => {
+  const LinearGradient =
+    Expo && Expo.LinearGradient ? Expo.LinearGradient : RNLinearGradient;
+
   const generated = generateGradient(gradient, {
     width: style.width,
     height: style.height
   });
+
   if (generated.length > 1) {
     return (
       <View style={[style, { position: "relative" }]}>
@@ -20,9 +26,12 @@ export default ({ gradient, children, style }) => {
       </View>
     );
   }
+
   return (
     <LinearGradient style={style} {...generated[0]}>
       {children || null}
     </LinearGradient>
   );
 };
+
+export default LinearGradientCss;
